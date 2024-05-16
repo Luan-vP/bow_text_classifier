e19cdfe41813fbccf77a9b951ab46f19abf7fa08
Luan van Pletsen <luanvanpletsen@gmail.com>
2024-05-16 17:01:17 +0100

    Add datasets and download side-effect

--- src/bow_text_classifier/data.py
+++ src/bow_text_classifier/data.py
@@ -1,3 +1,10 @@
+"""
+This module contains functions for downloading and parsing datasets.
+
+N.B. It contains a side-effect that downloads the dataset files to the data
+directory on import to ensure its availability.
+"""
+
 from logging import Logger
 from pathlib import Path
 
@@ -6,6 +13,12 @@ import requests
 logger = Logger(__name__)
 
 data_dir = Path(__file__).parent.parent.parent / "data"
+
+DATASETS = {
+    "dev": "https://raw.githubusercontent.com/neubig/nn4nlp-code/master/data/classes/dev.txt",
+    "train": "https://raw.githubusercontent.com/neubig/nn4nlp-code/master/data/classes/train.txt",
+    "test": "https://raw.githubusercontent.com/neubig/nn4nlp-code/master/data/classes/test.txt",
+}
 
 
 def download_dataset(url: str, output_file: Path | str):
@@ -20,5 +33,15 @@ def download_dataset(url: str, output_file: Path | str):
 
             with output_file.open("wb") as f:
                 f.write(response.content)
+    else:
+        logger.info(f"File already exists: {output_file}. Skipping download.")
 
     return output_file
+
+
+def get_dataset_filepath(dataset_name: str) -> Path:
+    return data_dir / "classes" / (dataset_name + ".txt")
+
+
+for dataset_name, url in DATASETS.items():
+    download_dataset(url, get_dataset_filepath(dataset_name))
