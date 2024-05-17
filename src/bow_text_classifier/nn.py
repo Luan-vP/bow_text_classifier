b57a89456bfdde310e134b5c594116b2f36582e6
Luan van Pletsen <luanvanpletsen@gmail.com>
2024-05-17 18:16:23 +0100

    Extract nn.train_bow()

--- src/bow_text_classifier/nn.py
+++ src/bow_text_classifier/nn.py
@@ -1,3 +1,5 @@
+import random
+
 import torch
 import torch.nn as nn
 
@@ -26,3 +28,46 @@ class BoW(torch.nn.Module):
 # function to convert sentence into tensor using word_to_index dictionary
 def sentence_to_tensor(sentence, word_to_index):
     return torch.LongTensor([word_to_index[_word] for _word in sentence.split(" ")])
+
+
+# perform training of the Bow model
+def train_bow(model, optimizer, criterion, train_data, test_data, type):
+    for ITER in range(10):
+        # perform training
+        model.train()
+        random.shuffle(train_data)
+        total_loss = 0.0
+        train_correct = 0
+        for sentence, tag in train_data:
+            sentence = torch.tensor(sentence).type(type)
+            tag = torch.tensor([tag]).type(type)
+            output = model(sentence)
+            predicted = torch.argmax(output.data.detach()).item()
+
+            loss = criterion(output, tag)
+            total_loss += loss.item()
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if predicted == tag:
+                train_correct += 1
+        # perform testing of the model
+        model.eval()
+        test_correct = 0
+        for _sentence, tag in test_data:
+            _sentence = torch.tensor(_sentence).type(type)
+            output = model(_sentence)
+            predicted = torch.argmax(output.data.detach()).item()
+            if predicted == tag:
+                test_correct += 1
+
+        # print model performance results
+        log = (
+            f"ITER: {ITER+1} | "
+            f"train loss/sent: {total_loss/len(train_data):.4f} | "
+            f"train accuracy: {train_correct/len(train_data):.4f} | "
+            f"test accuracy: {test_correct/len(test_data):.4f}"
+        )
+        print(log)
