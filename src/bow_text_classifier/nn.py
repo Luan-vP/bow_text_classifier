8939312943dd71272fc80fbc6561ddc74b9404f5
Luan van Pletsen <luanvanpletsen@gmail.com>
2024-05-17 16:17:34 +0100

    Add nn.py and BoW model

---
+++ src/bow_text_classifier/nn.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+# create a simple neural network with embedding layer, bias, and xavier initialization
+class BoW(torch.nn.Module):
+    def __init__(self, nwords, ntags):
+        super(BoW, self).__init__()
+        self.Embedding = nn.Embedding(nwords, ntags)
+        nn.init.xavier_uniform_(self.Embedding.weight)
+
+        type = (
+            torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+        )
+        self.bias = torch.zeros(ntags, requires_grad=True).type(type)
+
+    def forward(self, x):
+        emb = self.Embedding(x)
+        out = torch.sum(emb, dim=0) + self.bias
+        out = out.view(1, -1)
+        return out
