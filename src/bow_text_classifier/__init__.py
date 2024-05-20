85a834f5e88c0039aae9a977df779684d9b45edd
Luan van Pletsen <luanvanpletsen@gmail.com>
2024-05-20 21:58:22 +0100

    Initialize logger in __init__.py

--- src/bow_text_classifier/__init__.py
+++ src/bow_text_classifier/__init__.py
@@ -1,12 +1,16 @@
 import os
+from logging import Logger
 from pathlib import Path
 
 from .nn import BoW_Classifier
+
+logger = Logger(__name__)
 
 MODEL_PACKAGE_DIR = os.getenv("MODEL_PACKAGE_DIR", None)
 
 if MODEL_PACKAGE_DIR is not None:
     MODEL_PACKAGE_DIR = Path(MODEL_PACKAGE_DIR).resolve()
+    logger.info("Loading model from")
     if MODEL_PACKAGE_DIR.exists():
         model = BoW_Classifier()
         model.load_model(MODEL_PACKAGE_DIR)
